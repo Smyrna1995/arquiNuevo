6c5964547f9cd7342b373694367d0ef4d50544bc
MacaEsru <163170@ids.upchiapas.edu.mx>
2019-06-09 20:24:16 -0500

    add Modelo

--- Modelo/views.py
+++ Modelo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404
 
 from rest_framework.views import APIView
-from rest_framework.response import response
+from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import generics
 
